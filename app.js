3a736e5c0e341ea37e684068aaf604598c3271f4
edm1001 <edmer_franciz@yahoo.com>
2023-07-25 22:24:59 -0500

    add inquirer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+
+const pageHTML = generatePage(name,github);
 
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
